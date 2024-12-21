122c009f434ec029230396e6b2d631c261f3ed42
ameys28 <ameysawant2806@gmail.com>
2024-12-22 00:23:16 +0530

    .

--- backend/src/models/users.models.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true }, // Required username field
-  password: { type: String, required: true }, // Required password field
-  role: { type: String, enum: ['resident', 'security'], required: true } // Roles can be extended as needed
-});
-
-export default mongoose.model('User', userSchema);
